e08d9712526ec91f7e4ac038562373e0bf32a5bb
IswaryaAnandhan <aishwariyaanandhan@gmail.com>
2022-09-29 19:17:15 +0530

    added text and color

--- src/sample.js
+++ src/sample.js
@@ -1,7 +1,8 @@
 export default function Sample(){
     return <div>
-        <h1>Hello!</h1>
-        <h2> hi everyone!!</h2>
+        <h1>Hello!!!</h1>
+        <h2> hi everyone!</h2>
+        <h3>Thank you</h3>
         </div>
   
 }
