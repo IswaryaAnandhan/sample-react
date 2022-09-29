cdf92487ee772bf5bb118f258499220a3c428f04
IswaryaAnandhan <aishwariyaanandhan@gmail.com>
2022-09-29 22:19:28 +0530

    added text

--- src/sample.js
+++ src/sample.js
@@ -2,7 +2,7 @@ export default function Sample(){
     return <div>
         <h1>Hello!!!</h1>
         <h2> hi everyone!</h2>
-        <h3>Thank you</h3>
+        <h3>Welcome</h3>
         </div>
   
 }
